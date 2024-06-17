508d28b15e380249bd290ff9c7542ec924645aaa
Kaio Moreira <kaio-dev@outlook.com>
2024-06-17 12:11:10 -0300

    add: shipmentDAte for return item

--- src/repositories/prisma/prisma-items-repository.ts
+++ src/repositories/prisma/prisma-items-repository.ts
@@ -20,6 +20,7 @@ export class PrismaItemsRepository implements IItemsRepository{
                     select:{
                         delivery:{
                             select:{
+                                shippingDate: true,
                                 deliveryDate: true
                             }
                         }
