887eb3efad50ac969f609ba2f16d15fc71b64f87
Kaio Moreira <kaio-dev@outlook.com>
2024-07-03 11:13:43 -0300

    create: rote for return callback authorize melhor envio for admin

--- src/usecases/factories/orders/melhor-envio/make-authorize-melhor-envio-usecase.ts
+++ src/usecases/factories/orders/melhor-envio/make-authorize-melhor-envio-usecase.ts
@@ -1,4 +1,4 @@
-import { AuthorizeMelhorEnvioUsecase } from "@/usecases/orders/melhor-envio/authorize/authorize-melhor-envio-usecase.ts"
+import { AuthorizeMelhorEnvioUsecase } from "@/usecases/orders/melhor-envio/authenticate/authorize-melhor-envio-usecase.ts"
 
 export async function makeAuthorizeMelhorEnvio(): Promise<AuthorizeMelhorEnvioUsecase>{
         const authorizeMelhorEnvioUsecase  = new AuthorizeMelhorEnvioUsecase()
