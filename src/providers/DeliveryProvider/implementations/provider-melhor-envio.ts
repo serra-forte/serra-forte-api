af4861096282e8e0079b21c5bdf62af6ca9c2e88
Kaio Moreira <kaio-dev@outlook.com>
2024-09-06 14:49:01 -0300

    alter: logic for alter var env in railway per each reqest

--- src/providers/DeliveryProvider/implementations/provider-melhor-envio.ts
+++ src/providers/DeliveryProvider/implementations/provider-melhor-envio.ts
@@ -36,8 +36,6 @@ export class MelhorEnvioProvider implements IMelhorEnvioProvider {
 
   async shipmentCalculate(data: IRequestCalculateShipping): Promise<IResponseCalculateShipping[] | any> {
     try {
-      // await this.refreshToken();      
-
       const response = await axios.post(`${env.MELHOR_ENVIO_API_URL}/api/v2/me/shipment/calculate`, data, {
         headers: {
           'Authorization': `Bearer ${env.MELHOR_ENVIO_ACCESS_TOKEN}`,
@@ -46,6 +44,7 @@ export class MelhorEnvioProvider implements IMelhorEnvioProvider {
           'User-Agent': 'Serra Forte/kaiomoreira.dev@gmail.com',
         }
       });
+      console.log(env.MELHOR_ENVIO_ACCESS_TOKEN)
 
       if (response.status === 200) {  
         return response.data;
