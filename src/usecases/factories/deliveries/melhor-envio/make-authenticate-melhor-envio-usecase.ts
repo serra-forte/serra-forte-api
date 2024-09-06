167f208aa5ff86b0a2581838223b4da3e7046e2f
Kaio Moreira <kaio-dev@outlook.com>
2024-09-06 10:11:53 -0300

    add: log for debug authorization response

--- src/usecases/factories/deliveries/melhor-envio/make-authenticate-melhor-envio-usecase.ts
+++ src/usecases/factories/deliveries/melhor-envio/make-authenticate-melhor-envio-usecase.ts
@@ -1,8 +1,10 @@
 import { MelhorEnvioProvider } from "@/providers/DeliveryProvider/implementations/provider-melhor-envio"
+import { RailwayProvider } from "@/providers/RailwayProvider/implementations/provider-railway"
 import { AuthenticateMelhorEnvioUsecase } from "@/usecases/deliveries/melhor-envio/authenticate/authenticate-melhor-encio-usecase"
 
 export async function makeAuthenticate(): Promise<AuthenticateMelhorEnvioUsecase>{
-        const melhorEnvioProvider = new MelhorEnvioProvider()
+        const railwayProvider = new RailwayProvider()
+        const melhorEnvioProvider = new MelhorEnvioProvider(railwayProvider)
 
         const authenticateMelhorEnvioUsecase  = new AuthenticateMelhorEnvioUsecase(
             melhorEnvioProvider
